eb4fb572c0e35e9bf06ec3aff685fbeab3b22ff2
Joel Pulla <joel.pulla@outlook.com>
2024-08-23 11:51:39 -0500

    add pagination in getnowplayin ad filter for ultimate items

--- routes/movie.py
+++ routes/movie.py
@@ -1,13 +1,64 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from config.db import Session
+from datetime import datetime, timedelta
+
 from schema.movie_schema import Movie
 from models.movie_model import MovieModel
-from fastapi.responses import JSONResponse
 
 router = APIRouter()
+#Colsult All Movies
 
+### Goblar var pagination ###
+limit_results:int = 20
+
+
+@router.get('/movie/all_movie', tags=['movie'], status_code= 200, )
+def get_all_movies(page: int):
+    
+    db = Session()
+    total_results= db.query(MovieModel).count()
+    offset = (page-1)*limit_results
+    total_pages = (total_results // limit_results ) +(1 if total_results % limit_results > 0 else 0)
+    result = db.query(MovieModel).offset(offset).limit(limit_results).all()
+    
+    return {
+        "page":page,
+        "result": result,
+        "total_page": total_pages,
+        "total_results":total_results    
+    }
+
+@router.get('/search/movie', tags=['movie'])
+def search_movie_name(name: str)-> list[Movie]:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.movie_title.ilike(f"%{name}%")).all()
+    if not result:
+        return []
+    return result
+   
+#now Playind and pagination#   
+@router.get('/movie/now_playing', tags=['movie'])  
+def now_playing(page: int):
+    
+    db = Session()
+    total_results= db.query(MovieModel).count()
+    offset = (page-1)*limit_results
+    total_page = (total_results // limit_results)+ (1 if total_results % limit_results > 0 else 0)
+    
+    now_playing = db.query(MovieModel).order_by(MovieModel.created_at.desc()).offset(offset).limit(limit_results).all()
+    return {
+        "page": page,
+        "oofset": offset,
+        "results": now_playing,
+        "total items": total_results,
+        "total_pages": total_page
+    } 
+
+
+    
 #Cretae Movies
-@router.post('/movie', tags=['movie'], status_code=200, response_model= dict)
+@router.post('/movie/create', tags=['movie'], status_code=200, response_model= dict)
 def create_movie(movie: Movie) -> dict:
     db = Session()
     new_movie = MovieModel(**movie.model_dump())
@@ -16,27 +67,8 @@ def create_movie(movie: Movie) -> dict:
     
     return JSONResponse(status_code=200, content= {"messagge": "success"})
 
-#Colsult All Movies
-@router.get('/movie/all_movie', tags=['movie'], status_code= 200, )
-def get_all_movies(page: int):
-    
-    page_size:int = 10
-    db = Session()
-    total= db.query(MovieModel).count()
-    offset = (page-1)*page_size
-    
-    result = db.query(MovieModel).offset(offset).limit(page_size).all()
-    
-    return {
-        "page":page,
-        "result": result,
-        "total":total    
-    }
-
-
-
 #Delete Movies
-@router.delete('/movie/{id}',tags=['movie'])
+@router.delete('/movie/delete/{id}',tags=['movie'])
 def delete_movie(id:int):
     db = Session()
     
@@ -47,8 +79,7 @@ def delete_movie(id:int):
     db.commit()
     return JSONResponse(status_code=200, content={"message": "delete movie"})
 
-
-@router.put('/movie/{id}', tags=['movie'])
+@router.put('/movie/update/{id}', tags=['movie'])
 def update_movie(id: int, movie: Movie)-> dict:
     db = Session()
     result = db.query(MovieModel).filter(MovieModel.id == id).first()
@@ -70,12 +101,4 @@ def update_movie(id: int, movie: Movie)-> dict:
     db.commit()
     
     return JSONResponse( status_code=200, content={"message": "success"})
-    
-@router.get('/search/movie/', tags=['movie'])
-def search_movie_name(name: str)-> list[Movie]:
-    db = Session()
-    result = db.query(MovieModel).filter(MovieModel.movie_title.ilike(f"%{name}%")).all()
-    if not result:
-        return []
-    return result
     
